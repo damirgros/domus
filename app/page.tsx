9012fffef2100a172e408cc8c27911212d5bffb2
damirgros <grosdamir@gmail.com>
2026-08-03 17:41:49 +0200

    feat: implement workspace management in Home component with session handling and seeding

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,38 @@ import AboutUs from "../components/ui/landing_page/AboutUs";
 import Hero from "../components/ui/landing_page/Hero";
 import Footer from "../components/ui/landing_page/Footer";
 
-export default function Home() {
+import { cookies } from "next/headers";
+import { prisma } from "@/lib/prisma";
+import seed from "@/utils/demo-seed";
+
+export default async function Home() {
+  const cookieStore = await cookies();
+  const sessionId = await cookieStore.get("session")?.value;
+
+  let workspace = await prisma.workspace.findUnique({
+    where: {
+      sessionId,
+    },
+  });
+
+  if (!workspace && sessionId) {
+    const workspace = await prisma.workspace.create({
+      data: {
+        id: crypto.randomUUID(),
+        accessToken: crypto.randomUUID(),
+        sessionId,
+      },
+    });
+    if (workspace) {
+      seed(workspace)
+        .catch((error) => {
+          console.error("Seeding failed:", error);
+        })
+        .finally(async () => {
+          await prisma.$disconnect();
+        });
+    }
+  }
   return (
     <>
       <NavBar />
